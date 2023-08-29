d795506df12daf21c80380b9d3497bb7610e9e7a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-08-29 21:09:43 +0400

    Create AppUserRegisterValidator

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,21 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty!");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Surname cannot be empty!");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Username cannot be empty!");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email cannot be empty!");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty!");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Confirm password cannot be empty!");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password)
+                .WithMessage("Confirm password doesn't match the password you entered!");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Please enter a valid email address!");
+        }
+    }
+}
